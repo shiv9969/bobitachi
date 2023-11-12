36a42f954f496523f00b46d65a0dfad09264f134
betterbot69 <110813024+betterbot69@users.noreply.github.com>
2023-11-12 18:32:13 +0530

    tiny

--- pm_filter.py
+++ pm_filter.py
@@ -1658,7 +1658,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InputMediaPhoto(random.choice(PICS))
         )
         await query.message.edit_text(
-            text=script.premium_TXT,
+            text=script.PREMIUM_TXT,
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )    
